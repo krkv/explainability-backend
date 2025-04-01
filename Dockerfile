763ce22b0b98ba7652938eb0ef1ff947996bbca1
Rodion Krjutškov <rodion.krjutskov@gmail.com>
2025-04-01 22:15:50 +0300

    Improve app, Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,11 @@ WORKDIR /usr/local/app
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
+ENV FLASK_ENV=production
+ENV FLASK_APP=app.py
+
 # Copy in the source code
 COPY . .
 EXPOSE 5000
 
-CMD ["flask", "run", "--debug"]
+CMD ["gunicorn", "-w", "1", "-b", "0.0.0.0:8080", "app:app"]
--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request
 import json
+import os
 from dotenv import load_dotenv
 from assistant import generate_assistant_response
 
-load_dotenv()
-
 app = Flask(__name__)
+
+if os.getenv("FLASK_ENV") == "development":
+    app.logger.info("Loading the local env...")
+    load_dotenv()
+    
+@app.route('/ready', methods=['GET'])
+def ready():
+    return("OK", 200)
 
 @app.route('/getAssistantResponse', methods=['POST'])
 def get_response():
